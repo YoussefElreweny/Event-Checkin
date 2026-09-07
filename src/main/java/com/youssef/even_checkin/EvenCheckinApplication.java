76d175793e3835736ad33f3ce331dd280db5492e
Youssef Elrewenyyy <y_reweny@yahoo.com>
2026-09-07 12:47:11 +0300

    Starting

---
+++ src/main/java/com/youssef/even_checkin/EvenCheckinApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.even_checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenCheckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenCheckinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef/even_checkin/EvenCheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.even_checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenCheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
