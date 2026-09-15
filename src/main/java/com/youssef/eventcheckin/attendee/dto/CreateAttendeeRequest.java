77bd4a0a0e252051417ffb78a19583cff6501ab2
Youssef Elrewenyyy <y_reweny@yahoo.com>
2026-09-15 23:43:44 +0300

    nothig

--- src/main/java/com/youssef/eventcheckin/attendee/dto/CreateAttendeeRequest.java
+++ src/main/java/com/youssef/eventcheckin/attendee/dto/CreateAttendeeRequest.java
@@ -1,9 +1,7 @@
 package com.youssef.eventcheckin.attendee.dto;
 
-import com.youssef.eventcheckin.user.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateAttendeeRequest(
