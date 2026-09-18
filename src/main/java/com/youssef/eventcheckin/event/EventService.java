719b3ab2b8ad1bddb4a557889d0cf55e04379285
Youssef Elrewenyyy <y_reweny@yahoo.com>
2026-09-18 12:39:03 +0300

    h

--- src/main/java/com/youssef/eventcheckin/event/EventService.java
+++ src/main/java/com/youssef/eventcheckin/event/EventService.java
@@ -22,17 +22,12 @@ public class EventService {
     @Transactional
     public EventResponse create(CreateEventRequest request) {
 
-        if (!request.endsAt().isAfter(request.startsAt())) {
-            throw new RuntimeException("End time must be after start time");
-        }
-
         if (!request.checkInClosesAt().isAfter(request.checkInOpensAt())) {
             throw new RuntimeException("Check-in closing time must be after opening time");
         }
 
-        if (request.checkInOpensAt().isBefore(request.startsAt())
-                || request.checkInClosesAt().isAfter(request.endsAt())) {
-            throw new RuntimeException("Check-in window must be within the event time");
+        if (request.checkInClosesAt().isAfter(request.endsAt())) {
+            throw new RuntimeException("Check-in must close by the time the event ends");
         }
 
         User organizer = userRepository.findById(request.organizerId())
